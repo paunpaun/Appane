47f8964e4ebf5753e4505987f29c3d487d3fb759
STEFAN BOGDAN PAUN <paun.stefan@volta.ts.it>
2025-02-13 10:49:47 +0100

    styles

---
+++ back-office/back-office.php
@@ -0,0 +1,16 @@
+
+
+<html>
+    <head>
+
+    </head>
+    <body>
+
+    <?php
+    if (isset($_POST['username'])){
+        echo "<h1>LOGINNATO</h1>";
+    }
+?>
+        
+    </body>
+</html>
---
+++ back-office/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div>
+            <form action="back-office.php" method="POST">
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="password" placeholder="Password">
+                <input type="submit" placeholder="Login">
+            </form>
+        </div>
+    </body>
+</html>
