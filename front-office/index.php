341d0095338dedc82ee764b8c191105a9d8294a6
razinsta <razvanboss631@gmail.com>
2025-02-13 10:43:58 +0100

    Modified test into index.php

---
+++ front-office/index.php
@@ -0,0 +1,23 @@
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">logo</div>
+        <div class="header-text">text</div>
+    </header>
+    <div class="main-content">
+        body
+    </div>
+    <footer class="footer">
+        @serbanRazban Paunz
+    </footer>
+</body>
+</html>
