6705753b546e3bccf8e3feb3f8a4a125fd05c818
STEFAN BOGDAN PAUN <paun.stefan@volta.ts.it>
2025-02-13 11:03:55 +0100

    fonts

--- back-office/back-office.php
+++ back-office/back-office.php
@@ -3,6 +3,10 @@
 <html>
     <head>
 
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+
+
     </head>
     <body>
 
@@ -10,7 +14,10 @@
     if (isset($_POST['username'])){
         echo "<h1>LOGINNATO</h1>";
     }
-?>
-        
+    ?>
+
+    <h1>Title</h1>
+   
+
     </body>
 </html>
